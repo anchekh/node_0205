d22c4e6e64539be80f4cdac0c36bb6ae9104aa89
Dar1a <a.chekhh9@gmail.com>
2025-05-02 19:42:20 +1000

    backend-nodejs-24-web

---
+++ actor/ProductActor/ProductActor.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { Responder } from "../middleware/Responder";
+import { Pool } from "../middleware/Pool";
+
+export class ProductActor {
+    static async createProduct(req: Request, res: Response) {
+        try {
+            const {title, price, description} = req.body
+            const product = await Pool.conn.product.create({
+                data: {
+                    title, price, description
+                }
+            })
+            res.json(Responder.ok(product))
+        } catch (e) {
+            console.log(e)
+            res.json(Responder.internal())
+        }
+    }
+    static async createProductFeature(req: Request, res: Response) {
+        try {
+            const { product_id, title, icon, value } = req.body;
+        } catch (e) {
+            console.log(e);
+            res.json(Responder.internal());
+        }
+    }
+    static async createProductFilter(req: Request, res: Response) {
+        try {
+            const { title, type_id } = req.body;
+        } catch (e) {
+            console.log(e);
+            res.json(Responder.internal());
+        }
+    }
+    static async createProductFilterItem(req: Request, res: Response) {
+        try {
+            const { title, filter_id } = req.body;
+        } catch (e) {
+            console.log(e);
+            res.json(Responder.internal());
+        }
+    }
+    static async createProductImage(req: Request, res: Response) {
+        try {
+            const { src, product_id } = req.body;
+        } catch (e) {
+            console.log(e);
+            res.json(Responder.internal());
+        }
+    }
+    static async createProductType(req: Request, res: Response) {
+        try {
+            const { title, product_id } = req.body;
+        } catch (e) {
+            console.log(e);
+            res.json(Responder.internal());
+        }
+    }
+}
